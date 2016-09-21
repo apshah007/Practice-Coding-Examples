210a27970e7d684355610f8e96418ff5013f4428
Amy Shah <apshah007@yahoo.com>
2016-09-20 23:29:05 -0400

    Add files via upload
    Addition of a JS example about how to initialize an array

---
+++ array example Duckett.js
@@ -0,0 +1,16 @@
+// Code Examples from Treehouse and Books I found
+
+//creating an array  pg 71 Duckett
+
+var colors;
+colors = ['white', 'black', 'custom']; //creation of array literal 
+
+var el = document.getElementById('colors');
+el.text.Content = colors[0];
+
+var colors = newArray('white',  //using constructor format for array creation
+                       'black',
+                       'custom');
+
+var el = document. getElementById('colors');
+el.textContext = colors[0];
